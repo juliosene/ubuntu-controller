db25540fb4d347cbb22a5423b06e8c31efa2dbf2
Julio Sene França <julio.sene@gmail.com>
2021-05-11 11:57:58 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# create an auxiliar eth interface
+sudo ip link add dev dummy0 type dummy
+sudo ip link set dummy0 down
+sudo ip link set name eth53 dev dummy0
+sudo ip addr add 198.162.3.10/32 dev eth53
+sudo ip link set eth53 up
+# install pi hole
+# install samba
