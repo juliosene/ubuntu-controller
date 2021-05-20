3fc98acb221d62f6ab10f75708f2443e8572ae22
Julio Sene França <julio.sene@gmail.com>
2021-05-20 11:36:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
-#1 Edit network to create a VLAN
+# Install net-tools
+sudo apt -y update
+sudo apt install -y net-tools
+ifconfig
+# Edit network to create a VLAN
 # nano /etc/netplan/00-installer-config.yaml
-#2 Reboot
-#3 Install Pi-hole
+# Reboot
+# Install Pi-hole
 curl -sSL https://install.pi-hole.net | bash
 # Select the correct interface (VLAN) for pi hole
-#4 Set pihole web console password
+# Set pihole web console password
 sudo pihole -a -p
 # Hostname
 # sudo hostname piserverad
 # $ sudo nano /etc/hostname
-#5 install samba 4 as Active Directory
+# install samba 4 as Active Directory
 
